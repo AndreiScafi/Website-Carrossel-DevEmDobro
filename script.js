b092b122283a1f4054052ddb0e0355b460373039
Andrei Scafi <andreiscafi@hotmail.com>
2023-03-11 19:25:00 -0300

    Adicionando minha solução Javascript ao exercício.

---
+++ script.js
@@ -0,0 +1,27 @@
+const imagens = document.querySelectorAll('.imagem');
+const botoes = document.querySelectorAll('.botao');
+
+function removerClasseImagens() {
+    imagens.forEach(imagem => {
+        imagem.classList.remove('ativa')
+    });
+}
+
+function removerClasseBotoes() {
+    botoes.forEach(botao => {
+        botao.classList.remove('selecionado')
+    });
+}
+
+botoes.forEach((botao, index) => {
+    botao.addEventListener('click', () => {
+        removerClasseBotoes();
+        removerClasseImagens();
+
+        imagens[index].classList.add('ativa');
+        botoes[index].classList.add('selecionado');
+    })
+})
+
+
+
